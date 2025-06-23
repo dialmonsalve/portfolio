f1bdbc373574cca7736dc58680a1237c2d32e9ce
Diego Monsalve <diales1981@gmail.com>
2025-06-23 15:21:01 -0500

    Restructuring pages

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -21,6 +21,7 @@ const html = ({ name, email, description, phone }: Form) => `
   <p>Descripción: ${description} </p>
 `;
 
+
 const sendEmail = defineAction({
   accept: "form",
   input: z.object({
